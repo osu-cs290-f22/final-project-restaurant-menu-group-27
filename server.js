eab27ee53d2b045ff900a82ca9a258ca6b71f2aa
aaronducote <ducotea@oregonstate.edu>
2022-12-02 14:48:54 -0800

    server reads menu data with handlebars

--- server.js
+++ server.js
@@ -7,27 +7,47 @@ var express_handlebars = require('express-handlebars')
 var app = express()
 var port = process.env.PORT || 3000
 var fs = require("fs")
+var http = require("http")
+var menuData = require('./menuData')
 
 
 app.engine('handlebars', express_handlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
 
-
-app.get(['/', '/index.html'], function (req, res, next) {
-    fs.readFile("./public/index.html", function (error, data) {
-        console.log("  -- URL:", req.url)
-        res.writeHead(200, {
-            'Content-Type': 'text/html'                                     // Status code 200, content type HTML
-        })
-        res.write(data)                                                     // Writes data of page
-        res.end()
-    //res.status(200).render('postPage', {
-    //    post: postData
+app.get(['/', '/index.html'], function (req, res, next) {       // displays index.html
+    res.status(200).render('menuPage', {
+        post: menuData
     })
 })
 
+app.get('*', function (req, res) {
+    res.status(404).render('404', {})
+})
 
+
+/*var server = http.createServer(function (req, res) {                            // Begins server
+
+    if (req.url == ("/index.html")) {                                           // Checks against requested URL
+        fs.readFile("./public/index.html", function (error, data) {
+            console.log("  -- URL:", req.url)
+            res.writeHead(200, {
+                'Content-Type': 'text/html'                                     // Status code 200, content type HTML
+            })
+            res.write(data)                                                     // Writes data of page
+            res.end()
+        })
+    }
+})*/
+
+
+/*app.listen(port, function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("== Server listening on port", port);
+});*/
 
 app.listen(port, function (err) {
     if (err) {
