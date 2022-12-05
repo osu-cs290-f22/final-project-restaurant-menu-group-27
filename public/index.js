bef1dfd1c95cbe7a1c19c25a17ccc5819c457353
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2022-12-04 21:58:13 -0800

    client side work

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-//var menuData = require("../menuData.json")
+var allItems = document.querySelectorAll('.menu-items')
 
 
 /*      BUTTON VARIABLES AND EVENT LISTENERS        */
@@ -9,6 +9,7 @@ var breakfastButton = document.getElementById("breakfast-menu-button")
 var lunchButton = document.getElementById("lunch-menu-button")
 var dinnerButton = document.getElementById("dinner-menu-button")
 var specialButton = document.getElementById("specials-menu-button")
+var addButtons = document.querySelectorAll("#addto-cart-button")
 
 //display modal to user
 function customerCartButtonHandler(event) {
@@ -80,6 +81,22 @@ function specialButtonHandler(event) {
     event.stopPropagation()
 }
 
+//adds a menu item to the customer's cart
+function addButtonHandler(event) {
+    console.log("An add button was clicked")
+   
+    //get all data of clicked menu item
+    var url = event.target.parentNode.childNodes[1].src
+    var name = event.target.parentNode.childNodes[1].alt
+    var price = event.target.parentNode.childNodes[3].childNodes[3].innerText
+    var ingredients = event.target.parentNode.childNodes[3].childNodes[5].innerText
+    var reviews = event.target.parentNode.childNodes[3].childNodes[7].innerText
+
+    addToCart(name, url, price, ingredients, reviews)
+
+    event.stopPropagation()
+}
+
 cartButton.addEventListener("click", customerCartButtonHandler)
 buyButton.addEventListener("click", buyButtonHandler)
 exitButton.addEventListener("click", exitButtonHandler)
@@ -87,4 +104,22 @@ breakfastButton.addEventListener("click", breakfastButtonHandler)
 lunchButton.addEventListener("click", lunchButtonHandler)
 dinnerButton.addEventListener("click", dinnerButtonHandler)
 specialButton.addEventListener("click", specialButtonHandler)
-/****************************************************************/
+
+for (var x = 0; x < addButtons.length; x++) 
+    addButtons[x].addEventListener("click", addButtonHandler)
+/*************************************************************/
+
+//this function will add a specific menu item to the customer cart
+function addToCart(name, url, price, ingredients, reviews) {
+
+    var newItem = Handlebars.templates.modalCart({
+        name: name,
+        url: url,
+        price: price,
+        ingredients: ingredients,
+        reviews: reviews
+    })
+
+    var itemContainer = document.querySelector(".cart-item")
+    itemContainer.insertAdjacentHTML("beforeend", newItem)
+}
