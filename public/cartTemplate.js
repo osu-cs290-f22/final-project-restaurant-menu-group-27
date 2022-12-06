44e4d6003d0adc2ec643dcd3e1ce2a102e74d3bc
Fuminou <114370077+Fuminou@users.noreply.github.com>
2022-12-06 05:23:29 -0800

    worked on handlebar css and stuff

--- public/cartTemplate.js
+++ public/cartTemplate.js
@@ -8,7 +8,7 @@ templates['modalCart'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
         return undefined
     };
 
-  return "<div class=\"cart-item-background\">\r\n    <img class=\"food-image\" src="
+  return "<div class=\"item-background\">\r\n    <img class=\"food-image\" src="
     + alias4(((helper = (helper = lookupProperty(helpers,"url") || (depth0 != null ? lookupProperty(depth0,"url") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data,"loc":{"start":{"line":2,"column":32},"end":{"line":2,"column":39}}}) : helper)))
     + " alt="
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":2,"column":44},"end":{"line":2,"column":52}}}) : helper)))
@@ -20,6 +20,6 @@ templates['modalCart'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
     + alias4(((helper = (helper = lookupProperty(helpers,"ingredients") || (depth0 != null ? lookupProperty(depth0,"ingredients") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ingredients","hash":{},"data":data,"loc":{"start":{"line":6,"column":31},"end":{"line":6,"column":46}}}) : helper)))
     + "</p>\r\n        <p class=\"reviews\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"reviews") || (depth0 != null ? lookupProperty(depth0,"reviews") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"reviews","hash":{},"data":data,"loc":{"start":{"line":7,"column":27},"end":{"line":7,"column":38}}}) : helper)))
-    + "</p>\r\n    </div>\r\n</div>";
+    + "</p>\r\n    </div>\r\n    <button id=\"remove-cart-button\" class=\"remove-button\">Remove Item</button>\r\n</div>";
 },"useData":true});
 })();
