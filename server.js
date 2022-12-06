4ef58cdeac29340be9d637fe26f3580948ec1618
aaronducote <ducotea@oregonstate.edu>
2022-12-05 20:43:16 -0800

    Changed URL to /menu

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var express = require('express')
 var express_handlebars = require('express-handlebars')
 var app = express()
 var port = process.env.PORT || 3000
-var fs = require("fs")
-var http = require("http")
 var menuData = require('./menuData')
 
 
@@ -16,7 +14,7 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get(['/', '/index.html'], function (req, res, next) {       // displays index.html
+app.get(['/', '/menu'], function (req, res, next) {       // displays index.html
     res.status(200).render('menuPage', {
         breakfast: menuData.breakfast,
         lunch: menuData.lunch,
@@ -30,27 +28,6 @@ app.get('*', function (req, res) {
 })
 
 
-/*var server = http.createServer(function (req, res) {                            // Begins server
-
-    if (req.url == ("/index.html")) {                                           // Checks against requested URL
-        fs.readFile("./public/index.html", function (error, data) {
-            console.log("  -- URL:", req.url)
-            res.writeHead(200, {
-                'Content-Type': 'text/html'                                     // Status code 200, content type HTML
-            })
-            res.write(data)                                                     // Writes data of page
-            res.end()
-        })
-    }
-})*/
-
-
-/*app.listen(port, function (err) {
-    if (err) {
-        throw err;
-    }
-    console.log("== Server listening on port", port);
-});*/
 
 app.listen(port, function (err) {
     if (err) {
